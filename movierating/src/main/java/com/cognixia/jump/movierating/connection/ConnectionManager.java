d9c44c4b2844166c704860b2436b67a280340344
Jose Rivera <jpr47@njit.edu>
2024-02-07 15:44:20 -0500

    testing .gitignore with connection manager

--- movierating/src/main/java/com/cognixia/jump/movierating/connection/ConnectionManager.java
+++ movierating/src/main/java/com/cognixia/jump/movierating/connection/ConnectionManager.java
@@ -17,7 +17,7 @@ public class ConnectionManager {
 	private static final String URL = "jdbc:mysql://localhost:3306/movie_app";
 	private static final String USERNAME = "root";
     private static final String PASSWORD = "root"; // change password if needed, Mac/Linux = Root@123  |  Windows = root
-    //private static final String PASSWORD = "Root@123"; // change password if needed, Mac/Linux = Root@123  |  Windows = root
+   private static final String PASSWORD = "Root@123"; // change password if needed, Mac/Linux = Root@123  |  Windows = root
 	
 	private static Connection connection = null;
 	
