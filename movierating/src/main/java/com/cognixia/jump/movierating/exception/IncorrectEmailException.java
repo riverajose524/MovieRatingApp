851493a914ef70c69e915c160a3ec3a8d4f015e5
Jose Rivera <jpr47@njit.edu>
2024-02-08 12:31:51 -0500

    refactored name of exceptions package

--- movierating/src/main/java/com/cognixia/jump/movierating/exception/IncorrectEmailException.java
+++ movierating/src/main/java/com/cognixia/jump/movierating/exception/IncorrectEmailException.java
@@ -1,10 +1,10 @@
-package com.cognixia.jump.exception;
+package com.cognixia.jump.movierating.exception;
 
 public class IncorrectEmailException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
-		//construct the exception that is thrown when user is not found
+		//construct the exception that is thrown when incorrect email is entered
 		public IncorrectEmailException(String email) {
 			
 			super("The following: " + email + " is not a correctly written email");
