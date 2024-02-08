0b1af57526f6b7879a20ec04f3d99e2fe2e9ac1f
Nina Vdovicenco <nvdovic1@umbc.edu>
2024-02-08 12:09:06 -0500

    added movieRatingDao interface

---
+++ movierating/src/main/java/com/cognixia/jump/movierating/dao/MovieRatingDao.java
@@ -0,0 +1,25 @@
+package com.cognixia.jump.movierating.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.cognixia.jump.movierating.data.Movie;
+import com.cognixia.jump.movierating.data.MovieRating;
+
+public interface MovieRatingDao {
+	
+	 // needed for later so we make sure that the connection manager gets called
+    public void establishConnection() throws ClassNotFoundException, SQLException;
+
+    // as well, this method will help with closing the connection
+    public void closeConnection() throws SQLException;
+    
+    public boolean userLogIn(String username, String password);
+    
+    public List<Movie> getAllMovies();
+    
+    public MovieRating rateMovie(Movie selectedMovie, int rating);
+    
+    public void register(String username, String password);
+    
+}
