89bc955a26b64138cd168338b6a35f694667fa10
abashir191 <abdullahim.mohamed0@gmail.com>
2024-02-08 13:11:22 -0600

    added initial menu

---
+++ movierating/src/main/java/com/cognixia/jump/movierating/Main.java
@@ -0,0 +1,11 @@
+package com.cognixia.jump.movierating;
+
+
+public class Main {
+    public static void main(String[] args) {
+       	
+        Menu m = new Menu();
+        
+        m.mainMenu();
+    }
+}
