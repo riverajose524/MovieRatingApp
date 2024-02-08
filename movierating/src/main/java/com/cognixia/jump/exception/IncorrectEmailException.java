ad3ba18a1f8c22880e319f9167081184d6f1a123
riverajose524 <riverajose524@gmail.com>
2024-02-08 12:28:18 -0500

    Update IncorrectEmailException.java

--- movierating/src/main/java/com/cognixia/jump/exception/IncorrectEmailException.java
+++ movierating/src/main/java/com/cognixia/jump/exception/IncorrectEmailException.java
@@ -4,7 +4,7 @@ public class IncorrectEmailException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
-		//construct the exception that is thrown when user is not found
+		//construct the exception that is thrown when incorrect email is entered
 		public IncorrectEmailException(String email) {
 			
 			super("The following: " + email + " is not a correctly written email");
